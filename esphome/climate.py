d1cdd2787f8730e0e47f8cde0399a9826d0fccab
fraschizzato <fraschizzato@gmail.com>
2020-11-29 02:55:21 +0100

    Debugging Night

--- esphome/climate.py
+++ esphome/climate.py
@@ -331,7 +331,7 @@ class EsphomeClimateEntity(EsphomeEntity, ClimateEntity):
            await self._client.climate_command(key=self._static_info.key, away=False)
            await self._client.climate_command(key=self._static_info.key, night=False)
            await self._client.climate_command(key=self._static_info.key, boost=True)
-        if preset_mode == "night":
+        if preset_mode == "sleep":
            _LOGGER.error("Night IN", night)
            await self._client.climate_command(key=self._static_info.key, away=False)
            await self._client.climate_command(key=self._static_info.key, boost=False)
